9c08a80930121de3d9e70276c54a96bb6ed3e034
FYLSunghwan <sunghwan519@hotmail.com>
2019-03-08 14:22:30 +0900

    2019-03-08 14:22:21 Success

---
+++ BOJ/BOJ_5543.cpp
@@ -0,0 +1,10 @@
+//BOJ 5543
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    int a[5];
+    for (int i = 0; i < 5; i++) cin >> a[i];
+    cout << min({a[0], a[1], a[2]}) + min(a[3], a[4])-50;
+    return 0;
+}

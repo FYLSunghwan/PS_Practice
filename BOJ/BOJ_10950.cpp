d5b3bbd4de8962da572f0247d569c7254b72ccc4
FYLSunghwan <sunghwan519@hotmail.com>
2019-03-17 20:26:45 +0900

    2019-03-17 20:26:35 Success

---
+++ BOJ/BOJ_10950.cpp
@@ -0,0 +1,11 @@
+//BOJ 10950
+#include <cstdio>
+int c;
+int main() {
+    for(scanf("%d",&c);c;c--) {
+        int a,b;
+        scanf("%d%d",&a,&b);
+        printf("%d\n",a+b);
+    }
+    return 0;
+}
